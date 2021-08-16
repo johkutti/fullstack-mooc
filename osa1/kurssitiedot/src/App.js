5b5964feb730b7597b451fe74b17ca5658e26594
Johanna <jossusu@hotmail.com>
2021-08-16 21:24:12 +0300

    tehtävät 1.3-1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -9,10 +9,11 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props.part)
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.part['name']} {props.part['exercises']} 
       </p>
     </div>
   )
@@ -21,9 +22,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.osaset[0]} />
+      <Part part={props.osaset[1]} />
+      <Part part={props.osaset[2]} />
     </div>
   )
 }
@@ -31,31 +32,35 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.summa}</p>
+      <p>Number of exercises {props.osaset[0]['exercises']+props.osaset[1]['exercises']+props.osaset[2]['exercises']}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        part1={part1} 
-        part2={part2} 
-        part3={part3} 
-        exercises1={exercises1} 
-        exercises2={exercises2} 
-        exercises3={exercises3} />
-      <Total summa={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content osaset={course.parts}  />
+      <Total osaset={course.parts} />
     </div>
   )
 }
